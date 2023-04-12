624bd0e2cd2a11545e9a79f47aa94777395b1fac
Spartan <nimish.lingesh@gmail.com>
2023-04-11 17:30:24 -0700

    API framework

--- DEVELOPING.mg
+++ DEVELOPING.mg
@@ -3,3 +3,9 @@ Local macOS setup:
 Docker and kubernetes:
 https://medium.com/containers-101/local-kubernetes-for-mac-minikube-vs-docker-desktop-f2789b3cad3a
 
+
+BACKEND
+
+ENVIRONMENT SETUP TO RUN LOCALLY
+export python path:
+export PYTHONPATH="${PYTHONPATH}:/backend/api/src"
---
+++ backend/api/src/database/index.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+
+import databaseConfig from '../config/database';
+
+import User from '../app/models/User';
+// import Student from '../app/models/Student';
+// import Plan from '../app/models/Plan';
+// import Enrollment from '../app/models/Enrollment';
+// import Checkin from '../app/models/Checkin';
+// import HelpOrder from '../app/models/HelpOrder';
+
+// const models = [User, Student, Plan, Enrollment, Checkin, HelpOrder];
+const models = [User]
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
