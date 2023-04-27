f491dedcbf3c4d077664890fb5ebcf7cf7a03654
Devansh Bansal <devanshd0@gmail.com>
2023-04-26 17:32:49 -0700

    Redesigning Home Activity
    - Removed text views
    - Added login and sign up button

--- android/app/src/main/java/com/example/a202project/FirstFragment.kt
+++ android/app/src/main/java/com/example/a202project/FirstFragment.kt
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.login.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
