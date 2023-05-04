4ad17880fc6c4429bf03c399e09b9f0896d95dfc
Devansh Bansal <devanshd0@gmail.com>
2023-05-03 17:31:03 -0700

    Updating Main Fragment for employee

--- android/app/src/main/java/com/example/a202project/FirstFragment.kt
+++ android/app/src/main/java/com/example/a202project/FirstFragment.kt
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.login.setOnClickListener {
+        binding.enrollment.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
