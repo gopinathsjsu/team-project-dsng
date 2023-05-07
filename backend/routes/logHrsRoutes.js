28ad9cba4481afc876955c64903069cb6707d35d
Spartan <nimish.lingesh@gmail.com>
2023-05-06 18:12:42 -0700

    Customer checkout and log activities

--- backend/routes/logHrsRoutes.js
+++ backend/routes/logHrsRoutes.js
@@ -4,8 +4,8 @@ import { logCheckin, logCheckout, logActivity} from '../controllers/logHrsContro
 const router = express.Router();
 
 router.post('/checkin', logCheckin);
-// router.post('/checkout', logCheckout);
-// router.post('/activity', logActivity);
+router.post('/checkout', logCheckout);
+router.post('/activity', logActivity);
 // router.get('/:customerId', getCustomer);
 // router.delete('/:customerId', deleteCustomer);
 
